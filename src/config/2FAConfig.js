1de40ddd2f76f709e75f4c84cc99cc5cb07fabeb
DinoPimenteiro <gabrielwilliam234@gmail.com>
2025-09-23 15:04:05 -0300

    qrcode config maintence

--- src/config/2FAConfig.js
+++ src/config/2FAConfig.js
@@ -1,1 +1,23 @@
-import speakeasy from 'speakeasy';
+import { authenticator } from "otplib";
+import qrcode from "qrcode";
+
+export async function generateTotp(email, appName = "SimulAI") {
+  const secret = authenticator.generateSecret();
+  const token = authenticator.generate(secret);
+  const otppath = authenticator.keyuri(email, appName, secret);
+  const qrCodeLink = qrcode.toDataURL(otppath);
+  
+  return {
+    qrCodeLink,
+    secret,
+    token
+  };
+}
+
+export function verifyTOTP(secret, token){
+  const validate = authenticator.verify({ secret: secret, token: token });
+
+  if(!validate){
+    throw new Error("Invalid credentials.")
+  }
+}
